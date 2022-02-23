97bc63a1eb9ea359af2d5da735a51889c8941d10
Rafał Świerczek <swierczekraf@gmail.com>
2022-02-23 22:39:48 +0100

    Improve testing database connection

--- docker/php-fpm/scripts/check_connection.php
+++ docker/php-fpm/scripts/check_connection.php
@@ -2,22 +2,26 @@
 
 declare(strict_types=1);
 
+$dbUser = $argv[1];
+$dbPassword = $argv[2];
+$appEnv = $argv[3];
+
 try {
-    $pdo = new PDO("mysql:host=mysql;port=3306", $_ENV['DOCKER_DB_USER'], $_ENV['DOCKER_DB_PASSWORD']);
+    $pdo = new PDO("mysql:host=mysql;port=3306", $dbUser, $dbPassword);
 
     $stmt = $pdo->prepare('SHOW DATABASES');
     $stmt->execute();
 
     if (is_array($result = $stmt->fetchAll(PDO::FETCH_ASSOC))) {
         foreach ($result as $row) {
-            if ($row['Database'] === 'sylius_' . $_ENV['DOCKER_APP_ENV']) {
-                echo 1;
+            if ($row['Database'] === "sylius_$appEnv") {
+                echo 'connected';
                 exit;
             }
         }
     }
 
-    echo 0;
-} catch (\PDOException) {
-    echo 0;
+    echo "connected but couldn't find a database";
+} catch (\PDOException $ex) {
+    echo $ex->getMessage();
 }
